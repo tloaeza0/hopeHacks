f377cc50aa6d70bb10d3426e0541cf8e3fdd270b
Winston McHetz <winstonmchetz@R2Hs-MBP.attlocal.net>
2022-02-18 13:12:18 -0500

    dropdown list

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 const button = document.querySelector('.button')
-const inputValue = document.querySelector('.inputValue')
 const names = document.querySelector('.names')
 const desc = document.querySelector('.desc')
 const temp = document.querySelector('.temp')
+const apiKey = '301a67e1-6a30-415f-b3f4-c118cc060012'
 
 
 
-button.addEventListener('click',function(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=b234a2d9ac016e12f08ba1e3b70c5323')
+function fetchStates(){
+    fetch(`http://api.airvisual.com/v2/states?country=USA&key=${apiKey}`)
+        .then(res => res.json())
+        .then(res => {
+            const states = res.data.map(s => s.state)
+            const statesDataList = document.querySelector('#states')
+            states.forEach(stateName => {
+                const optionNode = document.createElement('option')
+                optionNode.setAttribute('value', stateName)
+                statesDataList.appendChild(optionNode)
+            });
+        })
+        .catch(err => console.log(err))
+}  
+fetchStates()
 
-.then(res => res.json())
-.then(data => {
-    const nameValue = data['name'];
-    const tempValue = `It is ${data.main.temp} degrees outside in ${data.name}`
-    const descValue = data['weather'][0]['description'];
 
-    names.innerHTML = nameValue;
-    desc.innerHTML = descValue;
-    temp.innerHTML = tempValue;
-    
+function fetchCities(state){
+    fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=${apiKey}`)
+        .then(res => res.json())
+        .then(res => {
+            const cities = res.data.map(c => c.city)
+            const citiesDataList = document.querySelector('#cities')
+            while (citiesDataList.firstChild) citiesDataList.firstChild.remove()
+            cities.forEach(cityName => {
+                const optionNode = document.createElement('option')
+                optionNode.setAttribute('value', cityName)
+                citiesDataList.appendChild(optionNode)
+            });
+        })
+        .catch(err => console.log(err))
+}
 
+state.addEventListener('change', e =>{
+    document.getElementById('city').value = ''
+    fetchCities(e.target.value)
 })
 
-.catch(err => alert('invalid search query'))
+
+city.addEventListener('change', e =>{
+    const state = document.getElementById('state').value
+    fetch(`http://api.airvisual.com/v2/city?city=${e.target.value}&state=${state}&country=USA&key=${apiKey}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.data.current.weather)
+            
+            const tempF = data.data.current.weather.tp * 9 / 5 + 32
+
+            names.innerHTML = `The temperature in ${e.target.value} is ${tempF} degrees`
+            
+        })
+        .catch(err => console.log(err))
 })

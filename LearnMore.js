5fcc7cee440661383d1cb4bef3ff7303ed5dab11
Winston McHetz <winstonmchetz@R2Hs-MBP.attlocal.net>
2022-02-21 20:40:48 -0500

    edits

--- LearnMore.js
+++ LearnMore.js
@@ -1,32 +1,24 @@
-
-
-/*--------Learn More JS---------*/
-var i = 0; 			// Start Point
-var images = [];	// Images Array
-var time = 4000;	// Time Between Switch
-	 
+var i = 0;          // Start Point
+var images = [];    // Images Array
+var time = 4000;    // Time Between Switch
 // Image List
 images[0] = "images/airpollution.jpg";
 images[1] = "images/vehicles-air-pollution.jpeg";
 images[2] = "images/citysmog.jpeg";
 images[3] = "images/citypollution.jpg";
-
 // Change Image
 function changeImg(){
-	document.slide.src = images[i];
-
-	// Check If Index Is Under Max
-	if(i < images.length - 1){
-	  // Add 1 to Index
-	  i++; 
-	} else { 
-		// Reset Back To O
-		i = 0;
-	}
-
-	// Run function every x seconds
-	setTimeout("changeImg()", time);
+    document.slide.src = images[i];
+    // Check If Index Is Under Max
+    if(i < images.length - 1){
+      // Add 1 to Index
+      i++;
+    } else {
+        // Reset Back To O
+        i = 0;
+    }
+    // Run function every x seconds
+    setTimeout("changeImg()", time);
 }
-
 // Run function when page loads
 window.onload=changeImg;
